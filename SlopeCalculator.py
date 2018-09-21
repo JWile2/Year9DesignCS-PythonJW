60d7a705be36fbf3889852468fd9d299f45fbe0e
Jules Wile <jules.wile@Y22-MYP-Design-Jules.local>
2018-09-21 13:52:11 -0400

    update misp

--- SlopeCalculator.py
+++ SlopeCalculator.py
@@ -1,5 +1,6 @@
+import os
 print("Slope Calculator:")
-
+os.system("say hello welcome to my slope calculator")
 #Input
 x1 = input("Input x1: ")
 x1 = int(x1)
@@ -17,6 +18,11 @@ y2 = int(y2)
 #Process
 rise = x2 - x1
 run = y2 - y1
+fslope = rise/run
 #Output
+print("Your slopeis m = "+str(rise)+"/"+str(run))
+print ("Your slope as a decimal is ",fslope)
 print(rise)
 print(run)
+os.system("say Your slope is m = "+str(rise)+"/"+str(run))
+os.system("say Your slope as a decimal is ",fslope)
