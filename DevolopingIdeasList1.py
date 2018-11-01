b63bb49e76c771e21e16f6950c356c6902166214
Jules Wile <jules.wile@Y22-MYP-Design-Jules.local>
2018-11-01 12:47:31 -0400

    Devoloping Ideas

---
+++ DevolopingIdeasList1.py
@@ -0,0 +1,9 @@
+#input
+  
+food1 = input("Input food 1: ")
+food2 = input("Input food 2: ")
+food3 = input("Input food 3: ")
+#process
+foods = [food1,food2,food3]
+#output
+print(foods)
--- Mathquiz.py
+++
@@ -1,1 +0,0 @@
-questions = ("2 x 5:""10", "25 x 5""125") for q in questions.keys(): user_answer = input(q) if questions.get(q) == user_answer: print("Correct:") else: print("Incorrect:")
