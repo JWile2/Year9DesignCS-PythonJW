01d6c3682760926f2bdb3612cf6dc25708e4a1b6
Jules Wile <jules.wile@Y22-MYP-Design-Jules.local>
2018-11-01 12:59:43 -0400

    Developing Ideas List 2

---
+++ DevelopingIdeasList2.py
@@ -0,0 +1,10 @@
+#input
+  
+food1 = input("Input food 1: ")
+food2 = input("Input food 2: ")
+food3 = input("Input food 3: ")
+#process
+foods = [food1,food2,food3]
+#output - We want to sort our list in our program
+foods.sort()
+print(foods)
