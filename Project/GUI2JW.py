42aec18c8da830e28a2abc537930a96f5f16a317
Jules Wile <jules.wile@Y22-MYP-Design-Jules.local>
2018-11-21 13:28:17 -0500

    Gui

--- Project/GUI2JW.py
+++ Project/GUI2JW.py
@@ -5,7 +5,7 @@ import tkinter as tk
 
 root = tk.Tk()
 
-titlelabel = tk.Label(root, text = "PASSWORD GENERATOR", font=("Helvetica",16))
+titlelabel = tk.Label(root, text = "MATH QUIZ", font=("Helvetica",16), background = "yellow")
 titlelabel.grid(row = 0, column = 0, columnspan = 2)
 
 
@@ -13,13 +13,13 @@ output = tk.Text(root, height = 10, width = 50)
 output.config(state = "disable")
 output.grid(row = 1, column = 0, columnspan = 2)
 
-word1Label = tk.Label(root, text = "Word 1", background = "red", foreground = "blue")
+word1Label = tk.Label(root, text = "7 x 7", foreground = "blue")
 word1Label.grid(row = 2, column = 0, sticky = "NESW")
 
-word2Label = tk.Label(root, text = "Word 2", background = "red", foreground = "blue")
+word2Label = tk.Label(root, text = "8 x 8 x 2", foreground = "blue")
 word2Label.grid(row = 3, column = 0)
 
-word3Label = tk.Label(root, text = "Word 3", background = "red", foreground = "blue")
+word3Label = tk.Label(root, text = "63 x 2", foreground = "blue")
 word3Label.grid(row = 4, column = 0)
 
 
@@ -32,7 +32,7 @@ ent2.grid(row = 3, column = 1)
 ent3 = tk.Entry(root)
 ent3.grid(row = 4, column = 1)
 
-btnGo = tk.Button(root, text = "GENERATE")
+btnGo = tk.Button(root, text = "CHECK ANSWERS")
 btnGo.grid(row = 5, column = 1,)
 
 
